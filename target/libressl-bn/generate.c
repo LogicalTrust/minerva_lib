9e650295c9610bebb7e2fbf6bfbe1511f8b136f9
Mateusz Kocielski <shm@digitalsun.pl>
2021-10-19 14:02:14 +0200

    Add random bn generators

--- target/libressl-bn/generate.c
+++ target/libressl-bn/generate.c
@@ -1,4 +1,7 @@
 #include <openssl/bn.h>
+#include <random.h>
+
+#define GEN_RAND_MAX (12)
 
 BIGNUM *
 bn_new(void)
@@ -10,6 +13,26 @@ bn_new(void)
     //BN_rand(x, 64, 0, 0);
     BN_one(x);
     return x;
+}
+
+/*
+ * bn_gen_rand - generates random BN, it uses internal minerva rand
+ * implementation which ensures it is determnistic
+ */
+
+BIGNUM *
+bn_gen_rand(void)
+{
+    int i, len;
+    uint8_t x[GEN_RAND_MAX];
+
+    len = (rand() % (GEN_RAND_MAX-1)) + 1;
+
+    for (i = 0; i < len; i++) {
+        x[i] = rand();
+    }
+
+    return BN_bin2bn(x, len, NULL);
 }
 
 void __sanitizer_cov_trace_pc(void) {
--- target/libressl-bn/generate.h
+++ target/libressl-bn/generate.h
@@ -1,3 +1,4 @@
 #include <openssl/bn.h>
 
 BIGNUM *bn_new(void);
+BIGNUM *bn_gen_rand(void);
